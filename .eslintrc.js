cb60a499673df1b9f1798b300c67f3c28aa0c33c
郭海林 <guohl@gistack.cn>
2022-08-23 15:36:33 +0800

    配置代码规范

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint', 'prettier', 'react-hooks'],
+    rules: {
+        semi: ['error', 'always'],
+        indent: ['error', 4],
+        quotes: ['error', 'single'],
+        'linebreak-style': ['error', 'unix'],
+        '@typescript-eslint/no-unused-vars': 0,
+        'comma-dangle': ['error', 'always-multiline'],
+    },
+};
